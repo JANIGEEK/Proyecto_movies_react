e9eb91d7d9039368a8111d5d3d611e99f625ab81
JANIGEEK <josep_angelo@hotmail.com>
2023-08-20 19:21:26 -0600

    Acceso a detalles de cada pelicula mediante API

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,16 +1,64 @@
-import React from "react"
-import movie from "./movie.json"
+import React, { useEffect, useState } from "react";
+//import movie from "./movie.json";
+import { useParams } from "react-router-dom";
+import { get } from "../utils/Api";
 
-export function MovieDetails(){
-    const imageUrl="https:image.tmbd.org/t/p/w500" + movie.poster_path
-    return (
+export function MovieDetails() {
+
+  
+  const {Id}  = useParams();
+
+  const [movie, setMovie] = useState(null);
+  //console.log(Id);
+
+  useEffect(() => {
+    get("&i=" + Id).then((data) => {
+      setMovie(data);
+      console.log(data)
+    });
+  }, [Id]);
+
+  if (!movie){
+    return null
+  }
+
+  return (
     <div className="container p-5  m-5 d-flex justify-content-around align-content-center flex-wrap">
-        <img src={imageUrl} alt={movie.title} className="col-6"/>
-        <div className="col-6">
-            <p><strong>Title: </strong>{movie.title}</p>
-            <p><strong>Genres: </strong>{movie.genres.map((genre)=>genre.name).join(", ")}</p>
-            <p><strong>Description: </strong>{movie.overview}</p>
-        </div>
+      <img src={movie.Poster} alt={movie.Title}  />
+      <div className="col-6">
+        <p>
+          <strong>Title: </strong>
+          {movie.Title}
+        </p>
+        <p>
+          <strong>Duration: </strong>
+          {movie.Runtime}
+        </p>
+        <p>
+          <strong>Year: </strong>
+          {movie.Year}
+        </p>
+        <p>
+          <strong>Type: </strong>
+          {movie.Type}
+        </p>
+        <p>
+          <strong>Genres: </strong>
+          {movie.Genre}
+        </p>
+        <p>
+          <strong>Rated: </strong>
+          {movie.Rated}
+        </p>
+        <p>
+          <strong>Director: </strong>
+          {movie.Director}
+        </p>
+        <p>
+          <strong>Description: </strong>
+          {movie.Plot}
+        </p>
+      </div>
     </div>
-    )
+  );
 }
