6991fccfbd8464ee9c617637c973153b71da2644
JANIGEEK <josep_angelo@hotmail.com>
2023-08-20 19:20:40 -0600

    Remplazo de rutas de acceso a la API

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export function Card({movie}){
+    
+
     return (
         <div className="col-md-3 my-2">
-            <Link to={"/details/"+ movie.id}>
+            <Link to={"/details/"+ movie.imdbID}>
             <div className="card">
                 
-                <img src={movie.poster_path} alt={movie.title} className="card-img-top" width="50"/>
+                <img src={movie.Poster} alt={movie.Title} className="card-img-top" width="40"/>
                 <div className="card-body">
-                    <h3>{movie.title}{movie.vote_average}</h3>
-                    <p>{movie.popularity}</p>
+                    <h3>{movie.Title}</h3>
+                    <h4>{movie.Year}</h4>
+                    <p>{movie.type}</p>
                 </div>
             </div>
             </Link>
